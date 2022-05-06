4b51abc62348e1341682ac3f5c6a58ad5aa4373b
Ritik Sahu <ritikrks@gmail.com>
2022-05-06 20:40:35 +0530

    added no login functionality

--- utils.js
+++ utils.js
@@ -1,23 +1,23 @@
 const sendRequest = require("./requests");
 
 function makeUrl(amount,page,per_page,keyword){
-    safe_url = `client_hires=1-9,10-&amount=${amount}-&payment_verified=1&duration_v3=ongoing&page=${page}&per_page=${per_page}&q=${encodeURI(keyword)}&sort=recency&user_location_match=2&t=0,1`
+    safe_url = `client_hires=1-9,10-&amount=${amount}-&page=${page}&per_page=${per_page}&q=${encodeURI(keyword)}&sort=recency&user_location_match=2`
     return `https://www.upwork.com/search/jobs/url?${safe_url}`
 }
 
-async function getJobDetails(job_id) {
+async function getJobDetails(job_id, is_login=false) {
     const url = `https://www.upwork.com/ab/jobs/search/jobdetails/visitor/${job_id}/details`
-    const details = await sendRequest(url);
+    const details = await sendRequest(url, is_login);
     return details;
 }
-async function getPageDetails(keyword,page, per_page=50) {
+async function getPageDetails(keyword,page, per_page=50, is_login= false) {
     const url = makeUrl(5000,page,per_page,keyword);
-    const details = await sendRequest(url);
+    const details = await sendRequest(url, is_login);
     return details;
 }
 
-async function testURL(url) {
-    const details = await sendRequest(url);
+async function testURL(url, is_login= false) {
+    const details = await sendRequest(url,is_login);
     return details;
 }
 
